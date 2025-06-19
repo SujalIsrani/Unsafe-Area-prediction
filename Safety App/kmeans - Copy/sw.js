97f5b6cdc82cb3aa69b13260934b95362bf8760c
Sujal Israni <128592707+SujalIsrani@users.noreply.github.com>
2025-06-19 13:49:22 +0530

    Delete Safety App/kmeans - Copy directory

--- Safety App/kmeans - Copy/sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("install", e =>{
-    e.waitUntil(
-        caches.open("static").then(cache =>{
-            return cache.addAll(["./", "styles.css", "logo192.png"]);
-        })
-    );
-});
